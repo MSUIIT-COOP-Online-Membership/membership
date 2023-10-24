16b3210a773b9cae599f0b3a84f42d7b8a578d43
Hattah10 <norhata.sampaco@g.msuiit.edu.ph>
2023-10-24 12:54:11 +0800

    add membership route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -125,6 +125,11 @@
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
+                    
+                    <div> 
+                        <p> I am norhata sampaco</p>
+                    </div>
+
                 </div>
             </div>
         </div>

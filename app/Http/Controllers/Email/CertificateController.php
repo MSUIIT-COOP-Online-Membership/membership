58c905f9d9c9bebc773b9c549e43f8bd49f0dae5
hananramss <hananramos19@gmail.com>
2023-11-16 22:55:37 +0800

    email changes added

--- app/Http/Controllers/Email/CertificateController.php
+++ app/Http/Controllers/Email/CertificateController.php
@@ -5,18 +5,28 @@ namespace App\Http\Controllers\Email;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use App\Models\User;
 use App\Mail\CertificateMail;
+use Illuminate\Support\Facades\Log;
 
 class CertificateController extends Controller
 {
     public function certificateEmail(){
-        $certificate_data =[
-            'name' => 'Hanan Ramos'
-        ];
 
+        $users = User::get();
 
-
-    Mail::to('hananramos16@gmail.com')->send(new CertificateMail($certificate_data));
-    echo ("Email Sent Successfully");
+        foreach($users as $user)
+        {
+            $certificate_data =[
+                'name' => $user->name,
+                'date' => '11/14/23',
+                'time' => '1:00PM',
+                'duration' => "15 minutes",
+            ];
+    
+            Mail::to($user->email)->send(new CertificateMail($certificate_data));
+        }
+        Log::info('Email was sent.');
+        return redirect()->back()->with("success","");
     }
 }
--- app/Http/Controllers/Email/SeminarController.php
+++ app/Http/Controllers/Email/SeminarController.php
@@ -6,18 +6,27 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\SeminarMail;
+use App\Models\User;
+use Illuminate\Support\Facades\Log;
 
 class SeminarController extends Controller
 {
-    public function seminarEmail(){
-        $seminar_data =[
-            'name' => "Hanan Ramos",
-            'date' => "September 29, 2024",
-            'time' => "1:00pm",
-            'duration' => "15 minutes",
-        ];
+    public function seminarEmail()
+    {
+        $users = User::get();
 
-        Mail::to('hananramos16@gmail.com')->send(new SeminarMail($seminar_data));
-        echo ("Email Sent Successfully");
+        foreach($users as $user)
+        {
+            $seminar_data =[
+                'name' => $user->name,
+                'date' => '11/14/23',
+                'time' => '1:00PM',
+                'duration' => "15 minutes",
+            ];
+    
+            Mail::to($user->email)->send(new SeminarMail($seminar_data));
+        }
+        Log::info('Email was sent.');
+        return redirect()->back()->with("success","");
     }
 }
